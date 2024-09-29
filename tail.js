6e301ab321f6bce3f26ca902ac70153c471019d0
murray-pocha <murray.pocha@gmail.com>
2024-09-29 16:03:34 -0600

    Adjusted tests to tail.js

--- tail.js
+++ tail.js
@@ -8,9 +8,15 @@ const assertEqual = function (actual, expected) {
 };
 
 const tail = function(array) {
-  return array.slice[1];
+  return array.slice(1);
 };
 
 const words = ["yo Yo", "Lighthouse", "Labs"];
 tail(words);
 assertEqual(words.length, 3);
+
+const words1 = tail([1]);
+assertEqual(words1.length, 0);
+
+const words2 = tail(["Howdy", "Lighthouse", "Labs"]);
+assertEqual(words2.length, 2);
