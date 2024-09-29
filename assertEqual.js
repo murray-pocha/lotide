b93b83b0941550795095ca0d3137d0ed1a8cc062
murray-pocha <murray.pocha@gmail.com>
2024-09-29 13:14:54 -0600

    adjusted logs with templates

--- assertEqual.js
+++ assertEqual.js
@@ -1,10 +1,13 @@
 const assertEqual = function (actual, expected) {
   if (actual === expected) {
-    console.log("🟢🟢🟢" + " Assertion Passed: " + "Lighthouse Labs" + " === " + "Bootcamp");
+    console.log(`🟢🟢🟢 Assertion Passed: ${actual} === ${expected}`);
   } 
   else {
-    console.log("🛑🛑🛑" + " Assertion Failed: " + "Lighthouse Labs" + " !== " + "Bootcamp");
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
   }
 };
 
 assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(6, 9);
+assertEqual(10, 10);
+assertEqual("Blue", "Blue");
