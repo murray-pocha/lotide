c1765f416bf334a37d25610e96b6158df1c87dd0
murray-pocha <murray.pocha@gmail.com>
2024-09-29 16:12:19 -0600

    error correction

--- tail.js
+++ tail.js
@@ -21,7 +21,7 @@ assertEqual(words1.length, 0);
 const words2 = tail(["Howdy", "Lighthouse", "Labs"]);
 assertEqual(words2.length, 2);
 
-const words3 = tail*([]);
+const words3 = tail([]);
 assertEqual(words3.length, 0);
 
 assertEqual(words2[0], "Lighthouse");
