ca3b2cc5015cd5f78583fc3cae35480e3042828e
murray-pocha <murray.pocha@gmail.com>
2024-10-01 14:36:09 -0600

    built function and tested

---
+++ assertArraysEqual.js
@@ -0,0 +1,32 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`🟢🟢🟢 Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+function eqArrays(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+}
+
+function assertArraysEqual(arr1, arr2) {
+  const isEqual = eqArrays(arr1, arr2);
+
+  if (isEqual) {
+    console.log(`🟢🟢🟢 Assertion passed: ${arr1} === ${arr2}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion failed: ${arr1} !== ${arr2}`);
+  }
+}
+
+assertArraysEqual([1, 2, 3], [5, 7, 8]);
+assertArraysEqual([1, 2, 3], [1, 2, 3]);
