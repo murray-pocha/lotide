4a13cc8ddfe9b080d291e0135ee85c1f75f900d8
murray-pocha <murray.pocha@gmail.com>
2024-10-01 12:49:04 -0600

    re sending update to git

--- eqArrays.js
+++ eqArrays.js
@@ -1,23 +1,24 @@
-const assertEqual = function (actual, expected) {
+const assertEqual = function(actual, expected) {
   if (actual === expected) {
     console.log(`🟢🟢🟢 Assertion Passed: ${actual} === ${expected}`);
-  } 
-  else {
-   console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
   }
 };
 
 function eqArrays(arr1, arr2) {
   if (arr1.length !== arr2.length) {
     return false;
-}
-for(let i = 0; i < arr1.length; i++) {
-  if (arr1[i] !== arr2[i]) {
-    return false;
   }
-}
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
   return true;
 }
 
-
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true)
+assertEqual(eqArrays(["a", "b", "c"], ["a", "b", "c"]), true);
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+assertEqual(eqArrays([1, "b", 5], ["1", "b", 5]), false);
+assertEqual(eqArrays([1, 2, 3], [1, 8, 4]), true);
