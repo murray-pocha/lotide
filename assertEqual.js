af4a5e3a1afadae36b22b208f544d5d60e588daa
murray-pocha <murray.pocha@gmail.com>
2024-10-21 13:22:30 -0600

    refactored

--- assertEqual.js
+++ assertEqual.js
@@ -1,13 +1,9 @@
-const assertEqual = function (actual, expected) {
+const assertEqual = function(actual, expected) {
   if (actual === expected) {
     console.log(`🟢🟢🟢 Assertion Passed: ${actual} === ${expected}`);
-  } 
-  else {
-   console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
   }
 };
 
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(6, 9);
-assertEqual(10, 10);
-assertEqual("Blue", "Blue");
+module.exports = assertEqual;
