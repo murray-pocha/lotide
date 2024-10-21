30566f735f62db7696e1f60d51c65c6127f8e455
murray-pocha <murray.pocha@gmail.com>
2024-10-21 13:42:44 -0600

    refactored tail

--- tail.js
+++ tail.js
@@ -1,27 +1,5 @@
-const assertEqual = function (actual, expected) {
-  if (actual === expected) {
-    console.log(`🟢🟢🟢 Assertion Passed: ${actual} === ${expected}`);
-  } 
-  else {
-   console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
 const tail = function(array) {
   return array.slice(1);
 };
 
-const words = ["yo Yo", "Lighthouse", "Labs"];
-tail(words);
-assertEqual(words.length, 3);
-
-const words1 = tail([1]);
-assertEqual(words1.length, 0);
-
-const words2 = tail(["Howdy", "Lighthouse", "Labs"]);
-assertEqual(words2.length, 2);
-
-const words3 = tail([]);
-assertEqual(words3.length, 0);
-
-assertEqual(words2[0], "Lighthouse");
+module.exports = tail;
