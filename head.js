f442cc2aefc82d8a45d3bb4e1c0d47baea6489d5
murray-pocha <murray.pocha@gmail.com>
2024-10-21 13:21:21 -0600

    refactored

--- head.js
+++ head.js
@@ -1,15 +1,7 @@
-const assertEqual = function (actual, expected) {
-  if (actual === expected) {
-    console.log(`🟢🟢🟢 Assertion Passed: ${actual} === ${expected}`);
-  } 
-  else {
-   console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
+const assertEqual = require('./assertEqual');
 
 const head = function(array) {
   return array[0];
 };
 
-assertEqual(head([5, 6, 7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+module.exports = head;
