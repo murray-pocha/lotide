743a9a31eee5fa36ae2b0e272e5afdca23ed81cd
murray-pocha <murray.pocha@gmail.com>
2024-10-21 13:53:42 -0600

    refactored

--- eqArrays.js
+++ eqArrays.js
@@ -1,12 +1,4 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`🟢🟢🟢 Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
-function eqArrays(arr1, arr2) {
+const eqArrays = function(arr1, arr2) {
   if (arr1.length !== arr2.length) {
     return false;
   }
@@ -16,8 +8,6 @@ function eqArrays(arr1, arr2) {
     }
   }
   return true;
-}
+};
 
-assertEqual(eqArrays(["a", "b", "c"], ["a", "b", "c"]), true);
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
-assertEqual(eqArrays([1, 2, 3], [1, 3, 5]), false);
+module.exports = eqArrays;
