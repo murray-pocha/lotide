fa55f449ec6731db92a273485d4b600242d35645
murray-pocha <murray.pocha@gmail.com>
2024-10-29 15:27:29 -0600

    built index and formatted

---
+++ index.js
@@ -0,0 +1,38 @@
+const head               = require('./head');
+const tail               = require('./tail');
+const middle             = require('./middle');
+const assertArraysEqual  = require('./assertArraysEqual');
+const assertEqual        = require('./assertEqual');
+const assertObjectsEqual = require('./assertObjectsEqual');
+const countLetters       = require('./countLetters');
+const countOnly          = require('./countOnly');
+const eqArrays           = require('./eqArrays');
+const eqObjects          = require('./eqObjects');
+const findKeyByValue     = require('./findKeyByValue');
+const flatten            = require('./flatten');
+const letterPositions    = require('./letterPositions');
+const without            = require('./without');
+const map                = require('./map');
+const takeUntil          = require('./takeUntil');
+const findKey            = require('./findKey');
+
+module.exports = {
+  head,
+  tail,
+  middle,
+  assertArraysEqual,
+  assertEqual,
+  assertObjectsEqual,
+  countLetters,
+  countOnly,
+  eqArrays,
+  eqObjects,
+  findKeyByValue,
+  flatten,
+  letterPositions,
+  without,
+  map,
+  takeUntil,
+  findKey,
+
+};
