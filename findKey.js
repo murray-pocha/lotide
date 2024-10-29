4489765b8d36a8f82d466cf64d58328ae6b0c3f3
murray-pocha <murray.pocha@gmail.com>
2024-10-29 14:28:50 -0600

    built function and tested

---
+++ findKey.js
@@ -0,0 +1,22 @@
+const assertEqual = require('./assertEqual');
+
+const findKey = function(obj, callback) {
+  for (let key in obj) {
+    if (callback(obj[key])) {
+      return key;
+    }
+  }
+  return undefined;
+
+};
+
+const names = {
+  'Bill': {sports: 4},
+  'Jim' : {sports: 1},
+  'Bob' : {sports: 2},
+  'Fred': {sports: 3}
+};
+
+assertEqual(findKey(names, x => x.sports === 1), 'Jim');
+assertEqual(findKey(names, x => x.sports === 3), 'Fred');
+assertEqual(findKey(names, x => x.sports !== 4), 'Jim');
